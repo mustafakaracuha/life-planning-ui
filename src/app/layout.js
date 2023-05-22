5d4816274f55e8c4d5f261b9899d046c19160600
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-05-22 10:15:34 +0300

    edit

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Web UI',
+  title: 'Life Planning Web UI',
 }
 
 export default function RootLayout({ children }) {
